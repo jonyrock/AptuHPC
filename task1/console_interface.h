8440ce1327a481456b45939951b8816271a839f6
jonyrock <ilms@live.ru>
2014-01-27 01:24:23 +0400

    interface fix, show command add, interface print stream

--- task1/console_interface.h
+++ task1/console_interface.h
@@ -11,13 +11,25 @@ class ConsoleInterface {
 public:
 
     ConsoleInterface(
-            boost::function< void (float) > addHandler,
-            boost::function< void (size_t) > killHandler
+            boost::function< void (ConsoleInterface&, float) > addHandler,
+            boost::function< void (ConsoleInterface&, size_t) > killHandler,
+            boost::function< void (ConsoleInterface&) > showHandler
             ) :
     m_addHandler(addHandler),
-    m_killHandler(killHandler) {
+    m_killHandler(killHandler),
+    m_showHandler(showHandler){
         printWelcome();
         startInterfaceLoop();
+    }
+
+    void printMessage(const string& message) {
+        cout << message;
+    }
+    
+    template<typename T>
+    ConsoleInterface& operator << (const T& str){
+        cout << str;
+        return *this;
     }
 
 private:
@@ -27,26 +39,37 @@ private:
         cout << "Commands available:" << endl;
         cout << "   add <seconds>" << endl;
         cout << "   kill <id>" << endl;
+        cout << "   show" << endl;
         cout << "   help" << endl;
         cout << "   exit" << endl;
+
+        cout << "--------------------------------------" << endl;
+        cout << "Examples with description: " << endl;
+        cout << "add 13.5 // Add new task which will sleep for 13.5 seconds" << endl;
+        cout << "kill 12  // Kill task with id 12" << endl;
+        cout << "show     // Show active tasks with their statuses in format:" << endl;
+        cout << "         //   <id>:(-<seconds>) - seconds remain" << endl;
+        cout << "         //   <id>:(<seconds>)  - seconds waiting for new task" << endl;
+        cout << "         // Output example: 7:(-12.0) 13:(-32.7) 14:(12.0)" << endl;
+        cout << "help     // Show this help" << endl;
+        cout << "exit     // Exit from this app" << endl;
     }
 
     void consumeLine(const string& line) {
         stringstream ss(line);
         string command;
         ss >> command;
-        
         if (command == "add") {
             float seconds = 0;
-            
+
             ss >> seconds;
-            
+
             if (seconds == 0) {
                 cout << "enter sleep duration in seconds please. Example: add 12.3" << endl;
                 return;
             }
-            
-            m_addHandler(seconds);
+
+            m_addHandler(*this, seconds);
             return;
         }
         if (command == "kill") {
@@ -56,8 +79,12 @@ private:
                 cout << "enter process id please. Example: kill 12" << endl;
                 return;
             }
-            
-            m_killHandler(id);
+
+            m_killHandler(*this, id);
+            return;
+        }
+        if(command == "show"){
+            m_showHandler(*this);
             return;
         }
         if (command == "help") {
@@ -69,8 +96,8 @@ private:
         if (command == "exit") {
             exit(0);
         }
-        
-        
+
+
 
     }
 
@@ -85,7 +112,8 @@ private:
         }
     }
 
-    boost::function<void (float) > m_addHandler;
-    boost::function<void (size_t) > m_killHandler;
+    boost::function< void (ConsoleInterface&, float) > m_addHandler;
+    boost::function< void (ConsoleInterface&, size_t) > m_killHandler;
+    boost::function< void (ConsoleInterface&) > m_showHandler;
 
 };
--- task1/main.cpp
+++ task1/main.cpp
@@ -2,20 +2,31 @@
 #include "cached_thread_pool.h"
 
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
-void addHandler(float seconds) {
-    cout << "add with " << seconds << endl;
+void addHandler(ConsoleInterface& ci, float seconds) {
+    stringstream ss;
+    ss << "add with " << seconds << endl;
+    ci << ss.str();
 }
 
-void killHandler(size_t id) {
-    cout << "kill with " << id << endl;
+void killHandler(ConsoleInterface& ci, size_t id) {
+    stringstream ss;
+    ss << "kill with " << id << endl;
+    ci << ss.str();
+}
+
+void showHandler(ConsoleInterface& ci) {
+    stringstream ss;
+    ss << "1:(-12.0) 12:(-32.7) 14:(12.0)" << endl;
+    ci << ss.str();
 }
 
 int main(int argc, char** argv) {
 
-    ConsoleInterface ci(addHandler, killHandler);
+    ConsoleInterface ci(addHandler, killHandler, showHandler);
 
     return 0;
 }
